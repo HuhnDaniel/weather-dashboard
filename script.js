5c7dee2878b21b4161859268b0993718dcf673ef
Daniel Moody-Huhn <60518682+HuhnDaniel@users.noreply.github.com>
2020-04-16 12:46:51 -0500

    City list saved in localstorage

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ var citySearch = document.getElementById("city-search");
 var cityArray = [];
 var apiKey = "40c8ddef7d6dcf0fa45ee70ad6205851";
 
+// If there is a previous city array, get it and populate the city list
+if (localStorage.getItem("cityArray") !== null) {
+	cityArray = localStorage.getItem("cityArray").split(",");
+}
+populateCityList();
+
 $(document).ready(function() {
 
 	// Look for city name input
@@ -26,8 +32,11 @@ function getCityWeather(input) {
 
 		// Check if city is on citylist already
 		if (!cityArray.includes(input)) {
+
 			// Only add a city to the city list if a response is recieved, so it is confirmed to be a real city
 			cityArray.push(input);
+			// 
+			localStorage.setItem("cityArray", cityArray.toString());
 			populateCityList();
 		}
 	});
@@ -37,10 +46,11 @@ function getCityWeather(input) {
 function populateCityList() {
 	$(".city-list").empty();
 
+	// Make a listing for each city name
 	cityArray.forEach(function(city) {
-		var newCity = $("<li>");
-		newCity.addClass("city");
-		newCity.text(city);
-		$(".city-list").prepend(newCity);
+		$(".city-list")
+			.prepend($("<li>")
+				.addClass("city")
+				.text(city));
 	});
 }
