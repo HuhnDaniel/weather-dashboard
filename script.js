c5dd8e83398518198377f9493a36a13c7d8ad4dc
Daniel Moody-Huhn <60518682+HuhnDaniel@users.noreply.github.com>
2020-04-16 12:33:18 -0500

    Function to populate city list

--- script.js
+++ script.js
@@ -9,11 +9,13 @@ $(document).ready(function() {
 		e.preventDefault();
 
 		getCityWeather(citySearch.value);
+
+		citySearch.value = "";
 	});
 });
 
+// Function to take input city and search for its weather
 function getCityWeather(input) {
-	console.log(input);
 	var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + input + "&appid=" + apiKey;
 
 	$.ajax({
@@ -21,5 +23,24 @@ function getCityWeather(input) {
 		method: "GET"
 	}).then(function(res) {
 		console.log(res);
+
+		// Check if city is on citylist already
+		if (!cityArray.includes(input)) {
+			// Only add a city to the city list if a response is recieved, so it is confirmed to be a real city
+			cityArray.push(input);
+			populateCityList();
+		}
+	});
+}
+
+// Function to populate searched city list
+function populateCityList() {
+	$(".city-list").empty();
+
+	cityArray.forEach(function(city) {
+		var newCity = $("<li>");
+		newCity.addClass("city");
+		newCity.text(city);
+		$(".city-list").prepend(newCity);
 	});
 }
